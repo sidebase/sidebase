e04dfc7e4a8fc971790647484c8c5ed7f8e689b9
BracketJohn <njonalik@protonmail.com>
2022-08-30 18:15:39 +0200

    make builds with antd work for both dev and prod

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,12 @@
 import { defineNuxtConfig } from 'nuxt'
+import { isProduction } from './helpers'
+
+// Needed to keep `ant-design-vue` running as of latest RC.8, see https://github.com/nuxt/framework/issues/6941#issuecomment-1229739856
+const transpile = ['lodash-es']
+if (isProduction) {
+  // For production build via rollup, we need to also transpile babel
+  transpile.push('@babel/runtime')
+}
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
@@ -22,10 +30,6 @@ export default defineNuxtConfig({
         },
       },
     },
-    // Needed to keep `ant-design-vue` running as of latest RC.8, see https://github.com/nuxt/framework/issues/6941#issuecomment-1229739856
-    transpile: [
-      'lodash-es',
-      '@babel/runtime',
-    ],
+    transpile,
   },
 })
