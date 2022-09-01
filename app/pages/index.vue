1240ce299075be72aba91717e3b33502f19b3347
Valia Fetisov <contact@valiafetisov.com>
2022-09-01 14:31:23 +0200

    small fixes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,13 +30,14 @@ const ratingValue = ref(5)
 
 <template>
   <div class="h-screen flex items-center justify-center">
-    <div class="flex flex-col">
+    <div class="w-96 flex flex-col">
       <h1 class="text-xl">
-        Welcome to the SideBase Nuxt 3 Fullstack Scaffold.
+        Welcome to <a href="https://github.com/sidestream-tech/sidebase" target="_blank">SideBase</a>!
       </h1>
       <p>We rate this scaffold with:</p>
       <a-rate v-model:value="ratingValue" class="mx-auto my-8" />
       <example-status
+        class="mt-5"
         :status-check="statusCheckResult"
       />
     </div>
