07990248aa2e68cf2c97cd624ce910cd6497a317
BracketJohn <njonalik@protonmail.com>
2022-09-15 12:54:29 +0200

    update minor deps, remove manual nuxtconfig import, add nuxt types as per nuxt/framework#7544, hotfix story until issue is resolved

--- app/env.d.ts
+++ app/env.d.ts
@@ -1,6 +1,7 @@
 // Types for histoire
 /// <reference types="@histoire/plugin-vue/components" />
-
+// Types for nuxt, to avoid reload
+/// <reference types="nuxt" />
 // To get correct types for `svg`s
 declare module '*.svg' {
   import type { DefineComponent } from 'vue'
