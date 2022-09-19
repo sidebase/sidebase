535876da92932719343997e588f944a3ce8521f0
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-09-19 18:59:26 +0200

    Added dark mode support

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { makeParser } from '@sidestream-tech/nuxt-sidebase-parse'
 import { responseSchemaHealthCheck } from '~/server/schemas/healthz'
-import SidebaseLogo from '~/assets/sidebase_logo_light_icon_font_white_bg_long.svg'
+import SidebaseLogoLight from '~/assets/sidebase_logo_light_icon_font_white_bg_long.svg'
+import SidebaseLogoDark from '~/assets/sidebase_logo_dark_icon_font_for_dark_bg_long.svg'
 
 const transform = makeParser(responseSchemaHealthCheck)
 const { data: statusCheck, refresh } = await useFetch('/api/healthz', { transform })
@@ -23,8 +24,9 @@ const ratingValue = ref(5)
 <template>
   <div class="flex h-screen items-center justify-center">
     <div class="flex w-4/6 max-w-lg flex-col">
-      <SidebaseLogo class="w-72 fill-blue-200" />
-      <h1 class="text-xl">
+      <SidebaseLogoLight class="w-72 fill-blue-200 dark:hidden" />
+      <SidebaseLogoDark class="hidden w-72 fill-blue-200 dark:block" />
+      <h1 class="text-xl dark:text-white">
         Welcome to <a href="https://github.com/sidestream-tech/sidebase" target="_blank">sidebase</a>!
       </h1>
       <p>sidebase is a production ready, fullstack typescript nuxt 3 starter. It has a lot of batteries like testing, DB ORM, API examples, ... included. The data you see below is fetched from an API that is connected to a running database. DB, Frontend and Backend all start with a single command: <b>npm run dev</b></p>
