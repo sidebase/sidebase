39c3e22fcc835dc728000978d49e28f9fd6cc653
Nils <njonalik@protonmail.com>
2022-10-21 16:10:40 +0200

    feat: move to sidebase, update urls, update badges (#69)

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@ const ratingValue = ref(5)
       <SidebaseLogoLight class="w-72 fill-blue-200 dark:hidden" />
       <SidebaseLogoDark class="hidden w-72 fill-blue-200 dark:block" />
       <h1 class="text-xl dark:text-white">
-        Welcome to <a href="https://github.com/sidestream-tech/sidebase" target="_blank">sidebase</a>!
+        Welcome to <a href="https://github.com/sidebase/sidebase" target="_blank">sidebase</a>!
       </h1>
       <p>sidebase is a production ready, fullstack typescript nuxt 3 starter. It has a lot of batteries like testing, DB ORM, API examples, ... included. The data you see below is fetched from an API that is connected to a running database. DB, Frontend and Backend all start with a single command: <b>npm run dev</b></p>
       <p>Make this starter your own - this page is just a boring placeholder. We rate this scaffold with:</p>
