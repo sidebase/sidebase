d286eb42293bd061d425bdd63d0013a597d8d27c
Nils <njonalik@protonmail.com>
2022-12-21 10:58:01 +0100

    feat: add CI smoke-test pipeline and bump minor deps (#89)
    * chore: bumps minor deps
    
    * chore: bumo vitest
    
    * feat: add ci pipeline that checks if dev and production builds are still working
    
    * fix: explicitly type events
    
    * feat: slightly bump ci time to ensure that server is ready
    
    * feat: bump ci time even further

--- server/api/example/[id].put.ts
+++ server/api/example/[id].put.ts
@@ -1,6 +1,7 @@
+import type { H3Event } from 'h3'
 import { usePrisma } from '@sidebase/nuxt-prisma'
 
-export default eventHandler(async (event) => {
+export default eventHandler(async (event: H3Event) => {
   const prisma = usePrisma(event)
   const { params } = event.context
 
