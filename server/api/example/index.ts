599c9eec0e48f934fa5b21d55762cd86987b6b89
BracketJohn <njonalik@protonmail.com>
2022-12-21 10:48:33 +0100

    fix: explicitly type events

--- server/api/example/index.ts
+++ server/api/example/index.ts
@@ -1,6 +1,7 @@
+import type { H3Event } from 'h3'
 import { usePrisma } from '@sidebase/nuxt-prisma'
 
-export default eventHandler((event) => {
+export default eventHandler((event: H3Event) => {
   const prisma = usePrisma(event)
 
   return prisma.example.findMany()
--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,8 +1,9 @@
+import type { H3Event } from 'h3'
 import { usePrisma } from '@sidebase/nuxt-prisma'
 
 const startupTime = new Date()
 
-const handler = eventHandler(async (event) => {
+const handler = eventHandler(async (event: H3Event) => {
   const prisma = usePrisma(event)
   try {
     await prisma.$queryRaw`SELECT 1;`
